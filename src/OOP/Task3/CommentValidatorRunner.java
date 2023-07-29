f262c2666b87429517808eec724d6d20526f8bbf
Nina Khavkina <n-khavkina@mail.ru>
2023-07-29 13:48:32 +0300

    OOP.Task3 done

--- src/OOP/Task3/CommentValidatorRunner.java
+++ src/OOP/Task3/CommentValidatorRunner.java
@@ -17,14 +17,14 @@ public class CommentValidatorRunner {
                 new NegativeTextAnalyzer(), new SpamAnalyzer(spamKeywords),
                 new TooLongTextAnalyzer(commentMaxLength)};
         String[] tests = new String[8];
-        tests[0] = "This comment is so good.";                            // OK
-        tests[1] = "This comment is so Loooooooooooooooooooooooooooong."; // TOO_LONG
-        tests[2] = "Very negative comment !!!!=(!!!!;";                   // NEGATIVE_TEXT
-        tests[3] = "Very BAAAAAAAAAAAAAAAAAAAAAAAAD comment with :|;";    // NEGATIVE_TEXT or TOO_LONG
-        tests[4] = "This comment is so bad....";                          // SPAM
-        tests[5] = "The comment is a spam, maybeeeeeeeeeeeeeeeeeeeeee!";  // SPAM or TOO_LONG
-        tests[6] = "Negative bad :( spam.";                               // SPAM or NEGATIVE_TEXT
-        tests[7] = "Very bad, very neg =(, very ..................";      // SPAM or NEGATIVE_TEXT or TOO_LONG
+        tests[0] = "This comment is so good.";
+        tests[1] = "This comment is so Loooooooooooooooooooooooooooong.";
+        tests[2] = "Very negative comment !!!!=(!!!!;";
+        tests[3] = "Very BAAAAAAAAAAAAAAAAAAAAAAAAD comment with :|;";
+        tests[4] = "This comment is so bad....";
+        tests[5] = "The comment is a spam, maybeeeeeeeeeeeeeeeeeeeeee!";
+        tests[6] = "Negative bad :( spam.";
+        tests[7] = "Very bad, very neg =(, very ..................";
         CommentValidatorRunner commentValidatorRunner = new CommentValidatorRunner();
         for (String test : tests) {
             System.out.println(commentValidatorRunner.checkLabels(textAnalyzers, test));
