4ff7f1326b1cec60bc44b78e0a44d0e840033963
Nina Khavkina <n-khavkina@mail.ru>
2023-08-01 12:43:30 +0300

    collection task2 done

--- src/Collection/Task2/DeleteDuplicates.java
+++ src/Collection/Task2/DeleteDuplicates.java
@@ -8,29 +8,26 @@ public class DeleteDuplicates {
         Random random = new Random();
 
         for (int i = 0; i < 20; i++) {
-            list.add(random.nextInt(1, 10));
+            list.add(random.nextInt(1, 11));
         }
 
         System.out.println("LinkedList до удаления повторяющихся чисел: " + list);
-        Set<Integer> set = new HashSet<>(list);
-        System.out.println("Hashset: " + set);
 
+        Collections.sort(list);
         Iterator<Integer> iterator = list.iterator();
-        for (int i = 0; i < list.size(); i++) {
-            for (int j = i + 1; j < list.size(); j++) {
-                if (list.get(i).equals(list.get(j))) {
-                    list.set(j, 0);
-                }
-            }
-        }
-
+        Integer lastElement = iterator.next();
         while (iterator.hasNext()) {
-            if (iterator.next() == 0) {
+            Integer nextElement = iterator.next();
+            if (lastElement.equals(nextElement)) {
                 iterator.remove();
             }
+            lastElement = nextElement;
         }
 
         System.out.println("LinkedList после удаления повторяющихся чисел: " + list);
-
     }
 }
+
+
+
+
