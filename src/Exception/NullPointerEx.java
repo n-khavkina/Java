dda83045281c4466624836c2093c6edd92aada7f
Nina Khavkina <n-khavkina@mail.ru>
2023-08-07 22:27:01 +0300

    exception task1 done

---
+++ src/Exception/NullPointerEx.java
@@ -0,0 +1,14 @@
+package Exception;
+
+public class NullPointerEx {
+    private static String string;
+
+    public static void main(String[] args) {
+        try {
+            String newString = string.toLowerCase();
+            System.out.println(newString);
+        } catch (NullPointerException e) {
+            System.out.println("Переменная string = null!");
+        }
+    }
+}
