d1fa8eabcd97e36128a347725ab4c8c4bb9c42a4
Nina Khavkina <n-khavkina@mail.ru>
2023-08-09 15:21:40 +0300

    Division

--- src/Exception/Task4/Division.java
+++ src/Exception/Task4/Division.java
@@ -2,36 +2,42 @@ package Exception.Task4;
 
 
 public class Division {
+    private static Double result = null;
 
     public static void main(String[] args) {
         try {
-            System.out.println(divide("50", "10", "0"));
+            System.out.println(divide("50"));
         } catch (ArithmeticException | NumberFormatException | ArrayIndexOutOfBoundsException e) {
             System.out.println(e.getMessage());
+            System.out.println(result);
         }
     }
 
     public static Double divide(String... arguments) throws ArithmeticException, ArrayIndexOutOfBoundsException,
             NumberFormatException {
-        Double res = null;
         if (arguments.length == 0 || arguments.length == 1) {
+            result = null;
             throw new ArrayIndexOutOfBoundsException("В аргументы нужно передать строки с числами! Минимум 2 числа!");
         }
         int[] array = new int[arguments.length];
         int index = 0;
         for (String s : arguments) {
-            if (Integer.parseInt(s) == 0) {
-                throw new ArithmeticException("Деление на 0!");
-            } else if (s.matches("[^0-9]+")) {
+            if (s.matches("[^0-9]+")) {
+                result = null;
                 throw new NumberFormatException("В аргументы нужно передать строки с числами!");
+            }
+            if (Integer.parseInt(s) == 0) {
+                result = null;
+                throw new ArithmeticException("Деление на 0!");
             } else {
                 array[index++] = Integer.parseInt(s);
-                res = (double) array[0];
+                result = (double) array[0];
                 for (int i = 1; i < array.length; i++) {
-                    res /= array[i];
+                    result /= array[i];
                 }
             }
         }
-        return res;
+        return result;
     }
 }
+
