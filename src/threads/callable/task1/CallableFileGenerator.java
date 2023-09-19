231756b69025cee262e8d11c381e7b95e4bbc619
Nina <n-khavkina@mail.ru>
2023-09-19 15:40:13 +0300

    task1 done

--- src/threads/callable/task1/CallableFileGenerator.java
+++ src/threads/callable/task1/CallableFileGenerator.java
@@ -11,8 +11,8 @@ public class CallableFileGenerator implements Callable {
     private static int number = 1;
     private File file;
     @Override
-    public List<String> call() throws Exception {
-        file = new File("src/threads/callable/task1/test" + number + ".txt");
+    public String call() throws Exception {
+        file = new File("Java/src/threads/callable/task1/test" + number + ".txt");
         file.createNewFile();
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(file))) {
             bw.write("java_" + number++);
@@ -21,21 +21,20 @@ public class CallableFileGenerator implements Callable {
             e.printStackTrace();
         }
         Thread.sleep(1000);
-        File[] files = new File("src/threads/callable/task1").listFiles();
-        List<String> list = new ArrayList<>();
-        for (File f : files) {
-            list.add(f.getName());
-        }
-        return list;
+        return file.getName();
     }
 }
 
 class Main {
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
+    public static void main(String[] args) throws ExecutionException, InterruptedException, TimeoutException {
         ExecutorService pool = Executors.newFixedThreadPool(3);
+        Set<Future<String>> set = new HashSet<>();
         for (int i = 0; i < 10; i++) {
             CallableFileGenerator callable = new CallableFileGenerator();
-            Future<List<String>> future = pool.submit(callable);
+            Future<String> future = pool.submit(callable);
+            set.add(future);
+        }
+        for (Future<String> future : set){
             System.out.println(future.get());
         }
         pool.shutdown();
