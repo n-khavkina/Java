d2ed3afdb1494dd231424659c38d4c7e21fb3d38
Nina <n-khavkina@mail.ru>
2023-09-19 16:49:47 +0300

    task2 done

--- src/threads/callable/task2/CallableNumberGenerator.java
+++ src/threads/callable/task2/CallableNumberGenerator.java
@@ -1,30 +1,36 @@
 package threads.callable.task2;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 import java.util.concurrent.*;
 
 public class CallableNumberGenerator implements Callable {
     private Random random = new Random();
     private List<Integer> list = new ArrayList<>();
     @Override
-    public List<Integer> call() throws Exception {
+    public String call() throws Exception {
         System.out.println(Thread.currentThread().getName());
         for (int i = 0; i < 10; i++) {
             list.add(random.nextInt(1, 100));
         }
-        Thread.sleep(1000);
-        return list;
+        int result = 0;
+        for (Integer i : list){
+            result+=i;
+        }
+
+        return String.valueOf(result) ;
     }
 }
 
 class Main {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         ExecutorService pool = Executors.newFixedThreadPool(3);
+        Set <Future<String>> set = new HashSet<>();
         for (int i = 0; i < 10; i++) {
             CallableNumberGenerator callable = new CallableNumberGenerator();
-            Future<List<Integer>> future = pool.submit(callable);
+            Future<String> future = pool.submit(callable);
+            set.add(future);
+        }
+        for (Future<String> future : set){
             System.out.println(future.get());
         }
         pool.shutdown();
