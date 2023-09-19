50d4b182e468769b60dbe65f1640f79b5c881470
Nina <n-khavkina@mail.ru>
2023-09-19 16:50:26 +0300

    task2 done

--- src/threads/callable/task2/CallableNumberGenerator.java
+++ src/threads/callable/task2/CallableNumberGenerator.java
@@ -6,6 +6,7 @@ import java.util.concurrent.*;
 public class CallableNumberGenerator implements Callable {
     private Random random = new Random();
     private List<Integer> list = new ArrayList<>();
+
     @Override
     public String call() throws Exception {
         System.out.println(Thread.currentThread().getName());
@@ -13,24 +14,24 @@ public class CallableNumberGenerator implements Callable {
             list.add(random.nextInt(1, 100));
         }
         int result = 0;
-        for (Integer i : list){
-            result+=i;
+        for (Integer i : list) {
+            result += i;
         }
 
-        return String.valueOf(result) ;
+        return String.valueOf(result);
     }
 }
 
 class Main {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         ExecutorService pool = Executors.newFixedThreadPool(3);
-        Set <Future<String>> set = new HashSet<>();
+        Set<Future<String>> set = new HashSet<>();
         for (int i = 0; i < 10; i++) {
             CallableNumberGenerator callable = new CallableNumberGenerator();
             Future<String> future = pool.submit(callable);
             set.add(future);
         }
-        for (Future<String> future : set){
+        for (Future<String> future : set) {
             System.out.println(future.get());
         }
         pool.shutdown();
